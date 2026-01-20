9e2cee39760ecb53a577406b9e3eb00aa620ee6c
dvillanueva2 <dvillanueva2@academic.rrc.ca>
2026-01-19 23:11:24 -0600

    Added proof of concept.

--- web-store-project/src/App.tsx
+++ web-store-project/src/App.tsx
@@ -1,11 +1,12 @@
 import './App.css'
-import HeaderSection from './components/web-store/header'
+import HeaderSection from './components/web-store/header';
+import PartSelector from './components/web-store/parts';
 
 function App() {
   return (
     <>
       <HeaderSection text={["Currently we do not have any data."]} />
-
+      <PartSelector text={"ram"}></PartSelector>
     </>
   )
 }
