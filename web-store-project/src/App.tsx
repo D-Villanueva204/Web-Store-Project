7550fe72c7773a5e8d5821736d8656a12db37828
Czedrick <cmarcelino2@rrc.ca>
2026-01-19 22:04:26 -0600

    Minor Update: Added code just for testing

--- web-store-project/src/App.tsx
+++ web-store-project/src/App.tsx
@@ -1,11 +1,12 @@
-import './App.css'
 import HeaderSection from './components/web-store/header'
+import partSpecifications from './components/web-store/specifications/specifications'
+import './App.css'
 
 function App() {
   return (
     <>
       <HeaderSection text={["Currently we do not have any data."]} />
-
+      <partSpecifications />
     </>
   )
 }
---
+++ web-store-project/src/components/web-store/specifications/specifications.tsx
@@ -0,0 +1,39 @@
+function partSpecifications({ text }: { text: string }) {
+    return (
+        <section className="part-specifications"> 
+            <div>
+                <h2>Part Specifications</h2>
+            </div>
+            <div>
+                <table>
+                    <tr>
+                        <th>Name</th>
+                        <td>AMD Ryzen 7 7800X3D</td>
+                    </tr>
+                    <tr>
+                        <th>Series</th>
+                        <td>Ryzen 7000 Series</td>
+                    </tr>
+                    <tr>
+                        <th>Core Count</th>
+                        <td>8 Cores</td>
+                    </tr>
+                    <tr>
+                        <th>Thread Count</th>
+                        <td>16 Threads</td>
+                    </tr>
+                    <tr> 
+                        <th>Base Clock</th>
+                        <td>4.2 GHz</td>
+                    </tr>
+                    <tr>
+                        <th>Boost Clock</th>
+                        <td>5 GHz</td>
+                    </tr>
+                </table>
+            </div>
+        </section>
+    )
+};
+
+export default partSpecifications;
