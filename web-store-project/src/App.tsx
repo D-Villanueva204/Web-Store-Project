7f3748973cff2d6ff067029465826cd54e044d73
dvillanueva2 <dvillanueva2@academic.rrc.ca>
2026-01-21 08:23:33 -0600

    FIX: Added changes to prevent Vercel crashes on deploy.

--- web-store-project/src/App.tsx
+++ web-store-project/src/App.tsx
@@ -1,15 +1,13 @@
 import './App.css'
 import HeaderSection from './components/web-store/header/header';
-import HeaderSection from './components/web-store/header'
 import PartSpecifications from './components/web-store/specifications/specifications'
-import { pcProducts } from './components/web-store/product_data/product_data'
 import './App.css'
 
 function App() {
   return (
     <>
       <HeaderSection selection={["Menu", "Sales", "About Us", "Compatibility Checker"]} />
-      <PartSpecifications pcProducts={pcProducts} />
+      <PartSpecifications />
     </>
   )
 }
