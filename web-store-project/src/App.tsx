0832127945c93008485b177a9560f9648799e48c
dvillanueva2 <dvillanueva2@academic.rrc.ca>
2026-01-20 22:59:43 -0600

    MAJOR: Revised Header Object to better fit Sprint requirements. Now includes proper css, directory, and list iteration.

--- web-store-project/src/App.tsx
+++ web-store-project/src/App.tsx
@@ -1,26 +1,10 @@
 import './App.css'
-import HeaderSection from './components/web-store/header';
-import CaseGenerator from './components/web-store/parts/case/caseGenerator';
-import CPUGenerator from './components/web-store/parts/cpu/cpuGenerator';
-import CoolerGenerator from './components/web-store/parts/cooler/coolerGenerator';
-import GPUGenerator from './components/web-store/parts/gpu/gpuGenerator';
-import MotherboardGenerator from './components/web-store/parts/mobo/MotherboardGenerator';
-import OSGenerator from './components/web-store/parts/os';
-import PSUGenerator from './components/web-store/parts/psu';
-import RamGenerator from './components/web-store/parts/ram';
+import HeaderSection from './components/web-store/header/header';
 
 function App() {
   return (
     <>
-      <HeaderSection text={["Currently we do not have any data."]} />
-      <CaseGenerator></CaseGenerator>
-      <CPUGenerator></CPUGenerator>
-      <CoolerGenerator></CoolerGenerator>
-      <GPUGenerator></GPUGenerator>
-      <MotherboardGenerator></MotherboardGenerator>
-      <OSGenerator></OSGenerator>
-      <PSUGenerator></PSUGenerator>
-      <RamGenerator></RamGenerator>
+      <HeaderSection selection={["Menu", "Sales", "About Us", "Compatibility Checker"]} />
     </>
   )
 }
--- web-store-project/test.ts
+++
@@ -1,4 +0,0 @@
-// import CaseSelector from "./src/components/web-store/parts/case/caseSelector.ts";
-import MotherboardSelector from "./src/components/web-store/parts/mobo/MotherboardSelector.ts";
-
-console.log(MotherboardSelector("MSI Z87-G55"));
