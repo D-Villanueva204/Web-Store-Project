70ef1bb46f468d19921a26fc1c55043aa5836e29
shuarob <erobleza2@academic.rrc.ca>
2026-01-28 09:31:03 -0600

    Added products on the shopping cart list including the category name and their price.

--- web-store-project/src/pages/cart-page.tsx
+++ web-store-project/src/pages/cart-page.tsx
@@ -1,8 +1,13 @@
 import { useNavigate } from "react-router-dom"
 import "../components/web-store/cart-section/cart-section.css"
+import { pcProducts } from "../components/web-store/product_data/product_data"
 
 function CartPage() {
   const navigate = useNavigate()
+  const cartItems = pcProducts.map(product => ({
+    name: product.name,
+    price: product.price
+  }))
   return (
     <div className="cart-page">
       <header>
@@ -10,8 +15,24 @@ function CartPage() {
           Shopping Cart
         </h1>
       </header>
-      <p>Your cart is currently empty.</p>
-
+        <div className="cart-table">
+        <table>
+          <thead> 
+            <tr>
+              <th>Name</th>
+              <th>Price</th>
+            </tr>
+          </thead>
+          <tbody>
+            {cartItems.map((item, index) => (
+              <tr key={index}>
+                <td>{item.name}</td>
+                <td>{item.price}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
       <button
          
         onClick={() => navigate("/")}>
