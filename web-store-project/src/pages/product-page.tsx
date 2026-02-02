3723897ab999871bcf032dcc1a99e33cdacd6cb8
Czedrick <cmarcelino2@rrc.ca>
2026-02-02 16:49:16 -0600

    Features: Added a product page with connected routes from the mainpage to switch between pages. Added a star rating that I found from youtube that will be refactored later on in sprint 3 for a review input from the user, also added a buy now button for one of my groupmates so they can use this for their universal state.

--- web-store-project/src/pages/product-page.tsx
+++ web-store-project/src/pages/product-page.tsx
@@ -1,9 +1,11 @@
 import { useState } from "react";
 import categoryMap, { loadCategory } from "../components/web-store/product_selector/product_selector";
+import StarRating from "../components/web-store/stars/StarRating";
 
 function ProductPage() {
     const [products, setProducts] = useState<any[]>([]);
     const categories = ["CASE", "COOLER", "CPU", "GPU", "MOBO", "PSU", "RAM", "STORAGE", "OS"];
+    const defaultRating = localStorage.getItem("starRating")
 
     return (
         <div>
@@ -26,6 +28,14 @@ function ProductPage() {
                         <p>In stock: {product.stock}</p>
                     </div>
                 ))}
+            </div>
+            <div>
+                <StarRating iconSize={50} defaultRating={defaultRating}/>
+            </div>
+            <div>
+                <form>
+                    <button type="button">Buy Now</button>
+                </form>
             </div>
         </div>
     )
