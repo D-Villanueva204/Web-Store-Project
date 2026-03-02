e19844bf467f24e58c67a137ce6bd2cfa9e52c09
dvillanueva2 <dvillanueva2@academic.rrc.ca>
2026-03-02 16:34:36 -0600

    Basic service structure added.

---
+++ web-store-project/src/components/web-store/services/productService.ts
@@ -0,0 +1,8 @@
+import { fetchAllParts, addNewCPU, addNewCase, addNewCooler, addNewGPU, addNewMOBO, addNewOS, addNewPSU, addNewRAM, addNewStorage, getTypeByID, updateStock } from "../repositories/productRepository";
+import type { Part, PartType, Case, Storage, Cooler, GPU, MOBO, OS, PSU, RAM } from "../repositories/PartTypes";
+
+// GetByType
+// GetByID
+// Make sure AddPart generates new ID.
+
+
