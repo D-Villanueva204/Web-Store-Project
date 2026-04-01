9ab2857b6c7a15b1b13e86d44089fa1d34a08b0a
shuarob <erobleza2@academic.rrc.ca>
2026-04-01 02:07:34 -0500

    Finalized orderRepository to just use API_BASE_URL.

--- web-store-project/frontend/src/apis/orderRepository.ts
+++ web-store-project/frontend/src/apis/orderRepository.ts
@@ -1,6 +1,6 @@
 import type { Order } from "../../../shared/types/order"
 
-const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000/api/v1'
+const API_BASE_URL = `${import.meta.env.VITE_API_BASE_URL}/api/v1`
 
 
 export async function getAllOrders(): Promise<Order[]> {
